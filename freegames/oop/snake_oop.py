c80aa4d68b58594d93543680ba63f4c2b555f4bb
Adorilson Bezerra <adorilson.bezerra@escolar.ifrn.edu.br>
2021-01-19 01:51:24 -0300

    Defining more some classes and exercise in OOP Snake game

--- freegames/oop/snake_oop.py
+++ freegames/oop/snake_oop.py
@@ -2,16 +2,17 @@
 
 Exercises
 
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to arrow keys.
+1. How do you make the SnakeFast or SnakeSlow classes?
+2. How do you make a SnakeSmart, that change the direction when collide with edges?
+3. How would you make a new food types? When snake eat them it will more fast or decrease?
+4. How do you create a Actor that will be the Head and Food superclass?
 """
 
 from turtle import setup, hideturtle, tracer, listen, onkey, done, update, clear, ontimer
 from random import randrange, choice
 from freegames import square, vector
 
+# 4. How do you create a Actor that will be the Head and Food superclass?
 class Actor:
     def __init__(self, x, y):
         self.position = vector(x, y)
@@ -31,6 +32,7 @@ class Food(Actor):
     color = 'Blue'
     cal = 5
 
+#3. How would you make a new food types? When snake eat will more fast or decrease?
 class Apple(Food):
     color = 'Green'
     cal = 10
@@ -44,29 +46,71 @@ class BadFood(Food):
     cal = -5
 
 class Snake:
+    SPEED = 1
     def __init__(self, x=0, y=0):
         self.head = Head(x, y)
         self.body = [vector(10, 0)]
-        self.aim = vector(0, -10)
+        self.aim = vector(0*self.SPEED, -10*self.SPEED)
+
+        self.status = 'LIVE'
 
     def eat(self, food):
         print('snake is eating', food.cal)
         for x in range(food.cal):
-            self.body.append(self.head)
+            self.body.append(self.head.position)
+
+        for x in range(food.cal, 0):
+            del self.body[0]
 
     def change(self, x, y):
         "Change snake direction."
         print('snake is changing')
-        self.aim.x = x
-        self.aim.y = y
+        self.aim.x = x*self.SPEED
+        self.aim.y = y*self.SPEED
 
     def move(self):
         "Move snake forward one segment."
-        self.head = self.body[-1].copy()
-        self.head.move(self.aim)
+        self.head = Head(*self.body[-1].copy())
+        self.head.position.move(self.aim)
 
-        self.body.append(self.head)
-        self.body.pop(0)
+        if self.is_colliding_with_border():
+            self.on_collision_with_border()
+        elif self.is_eating_himself():
+            self.on_eating_himself()
+        else:
+            self.body.append(self.head.position)
+            self.body.pop(0) # cut the tail
+
+    def on_collision_with_border(self):
+        self.dead()
+
+    def on_eating_himself(self):
+        self.dead()
+
+    def is_eating_himself(self):
+        print(self.head.position, self.body)
+        return (self.head.position in self.body)
+
+    def dead(self):
+        self.status = 'DEAD'
+
+    def alive(self):
+        return self.status != 'DEAD'
+
+    def is_colliding_with_border(self):
+       return not(-200 < self.head.x < 190 and -200 < self.head.y < 190)
+
+# 1. How do you make the SnakeFast or SnakeSlow classes?
+class SnakeFast(Snake):
+    SPEED = 2
+
+class SnakeSlow(Snake):
+    SPEED = 0.5
+
+#2. How do you make a SnakeSmart, that change the direction when collide with edges?
+class SnakeSmart(Snake):
+    def on_collision_with_border(self):
+        self.change(10, 0)
 
 class GameSnake:
     def __init__(self):
@@ -79,12 +123,15 @@ class GameSnake:
         onkey(lambda: self.snake.change(0, -10), 'Down')
 
     def new_food(self):
-        food = choice((Food(x=0, y=0), Apple(x=0, y=0), Mouse(x=0, y=0)))
+        foods = [Food,
+                Apple,
+                Mouse,
+                BadFood,
+                ]
+        type_food = choice(foods)
+        food = type_food(0, 0)
         food.position = vector(randrange(-15, 15) * 10, randrange(-15, 15) * 10)
         return food
-
-    def on_collision_with_border(self):
-       return not(-200 < self.snake.head.x < 190 and -200 < self.snake.head.y < 190)
 
     def run(self):
         clear()
@@ -95,17 +142,16 @@ class GameSnake:
 
         self.snake.move()
 
-        if self.snake.head == self.food.position:
+        if self.snake.head.position == self.food.position:
             self.snake.eat(self.food)
             self.food = self.new_food()
 
-        if self.on_collision_with_border():
-            print('collide with border. game over.')
-            square(self.snake.head.x, self.snake.head.y, 9, 'red')
-            update()
-            return
-        else:
+        if self.snake.alive():
             ontimer(self.run, 100)
+        else:
+            print('>>> SNAKE IS DEAD <<<')
+            square(self.snake.head.x, self.snake.head.y, 9, 'red')
+            return
 
 def init():
     setup(420, 420, 370, 0)
